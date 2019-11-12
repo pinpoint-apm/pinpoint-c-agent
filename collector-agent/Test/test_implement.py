87097a8efe7641c843c7fe39cf30c5779c82bab6
liumingyi <liumingyi@navercorp.com>
2019-11-12 17:42:11 +0800

    [11-12] add consistency test

--- collector-agent/Test/test_implement.py
+++ collector-agent/Test/test_implement.py
@@ -46,20 +46,24 @@ class TestImplement(TestCase):
         from Common.Config import CAConfig
         collector_conf = CollectorAgentConf(CAConfig)
         app_management = AppManagement(collector_conf)
-        while True:
+        i =10000
+        while i >0:
             # stime =1573438683
-            span = '{"name":"PHP Request","FT":1500,' \
-                   '"server":"10.34.130.79:28081",' \
-                   '"sid":"3345567788","psid":"3345567789","tid":"phpmyadmin^1560951035971^1",' \
-                   '"S":1573438683,"E":20,' \
-                   '"clues":["46:200"],' \
-                   '"uri":"/index.html",' \
-                   '"ERR":{"msg":"23456789op[xdcfvgbhnjmk"},' \
-                   '"EC":1, "estr":"DIY",' \
-                   '"calls":[{"name":"hello","S":1573438683,"E":8,"calls":[{"name":"hello2","1573438683":2,"E":2,"clues":["-1:null","14:2019/06/25"],"calls":[{"name":"hello3","S":1573438683,"E":4}]}]}],' \
-                   '"client":"10.10.10.10"}'
+            # span = '{"name":"PHP Request","FT":1500,' \
+            #        '"server":"10.34.130.79:28081",' \
+            #        '"sid":"3345567788","psid":"3345567789","tid":"phpmyadmin^1560951035971^1",' \
+            #        '"S":1573438683,"E":20,' \
+            #        '"clues":["46:200"],' \
+            #        '"uri":"/index.html",' \
+            #        '"ERR":{"msg":"23456789op[xdcfvgbhnjmk"},' \
+            #        '"EC":1, "estr":"DIY",' \
+            #        '"calls":[{"name":"hello","S":1573438683,"E":8,"calls":[{"name":"hello2","1573438683":2,"E":2,"clues":["-1:null","14:2019/06/25"],"calls":[{"name":"hello3","S":1573438683,"E":4}]}]}],' \
+            #        '"client":"10.10.10.10"}'
+            span ='''{"AP":"t=10 D=10 i=10 b=10","E":1,"FT":1500,"S":1573549175197,"appid":"app-2","appname":"APP-2","calls":[{"E":1,"S":0,"calls":[{"E":0,"S":1,"clues":["-1:Array()","14:ture"],"name":"date","stp":"1501"}],"clues":["-1:Array","14:ture"],"name":"AppDate::outputDate","stp":"1501"}],"client":"10.34.130.156","clues":["46:200"],"name":"PHP Request","server":"10.34.130.134:8001","sid":"213111260","stp":"1500","tid":"app-2^1573547333369^13794","uri":"/index.php?type=get_date"}'''
             app_management.handle_front_agent_data(None, None, span.encode())
-            time.sleep(3)
+            i-= 1
+            # time.sleep(3)
             # gevent.sleep(1)
-
+        while True:
+            gevent.sleep(1)
 # GRPC_TRACE=all;GRPC_VERBOSITY=DEBUG
