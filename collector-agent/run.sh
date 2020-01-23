2a36eabc1667409ee2ada505a03b1f6be9e1f375
liumingyi <liumingyi@navercorp.com>
2020-01-23 11:24:27 +0800

    free the res when module exit

--- collector-agent/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-PPATH=$(dirname "$0")
-#export GRPC_TRACE=all
-#export GRPC_VERBOSITY=INFO
-#export GODEBUG=http2debug=1 
-export GRPC_ENABLE_FORK_SUPPORT=1
-#python -m plop.collector -f flamegraph $PPATH/run.py
-python $PPATH/run.py
