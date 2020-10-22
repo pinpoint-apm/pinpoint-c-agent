8cba908bbf3b0bdf58763349edec6c74a786d80c
eeliu <eeliu2009@gmail.com>
2020-10-22 17:13:36 +0800

    add python 2.7 plugins

---
+++ plugins/PY2.7/pinpoint/plugins/common/PinpointCommon.py
@@ -0,0 +1,50 @@
+﻿import random
+import pinpointPy
+from .PinpointDefine import *
+from pinpoint.settings import *
+
+class Candy(object):
+    def __init__(self,class_name,module_name):
+        self.class_name = class_name
+        self.module_name = module_name
+
+
+    def onBefore(self,*args, **kwargs):
+        pinpointPy.start_trace()
+        return (args,kwargs)
+
+    def onEnd(self,ret):
+        pinpointPy.end_trace()
+
+    def onException(self,e):
+        raise NotImplementedError()
+
+    def __call__(self, func):
+        self.func_name=func.__name__
+        def pinpointTrace(*args, **kwargs):
+            ret = None
+            # print("start", self.func_name)
+            args, kwargs = self.onBefore(*args, **kwargs)
+            try:
+                ret = func(*args, **kwargs)
+                return ret
+            except Exception as e:
+                self.onException(e)
+                raise e
+            finally:
+                # print("end", self.func_name)
+                self.onEnd(ret)
+
+        return pinpointTrace
+
+    def generateTid(self):
+        return ('%s^%s^%s') % (APP_ID,str(pinpointPy.start_time()), str(pinpointPy.unique_id()))
+
+    def generateSid(self):
+        return str(random.randint(0,2147483647))
+
+    def getFuncUniqueName(self):
+        if self.class_name:
+            return '%s\%s.%s'%(self.module_name,self.class_name,self.func_name)
+        else:
+            return '%s\%s'%(self.module_name,self.func_name)
---
+++ plugins/PY2.7/pinpoint/plugins/common/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by eeliu at 8/20/20
+from .PinpointCommon import *
+from .PinpointDefine import *
+from .Span import *
+from .Interceptor import Interceptor
