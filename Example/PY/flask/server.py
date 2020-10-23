8c5498acdef8ef437ae5962f028d6c1292306654
eeliu <eeliu2009@gmail.com>
2020-10-23 11:35:55 +0800

    remove example

--- Example/PY/flask/server.py
+++
@@ -1,8 +0,0 @@
-# server.py
-
-from wsgiref.simple_server import make_server
-from test import application
-
-httpd = make_server('', 8001, application)
-print('Serving HTTP on port 8000...')
-httpd.serve_forever()
