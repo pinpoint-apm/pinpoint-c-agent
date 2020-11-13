9cbb7799c443cd3596281f377a99b57d430cc747
eeliu <eeliu2009@gmail.com>
2020-11-13 17:09:03 +0800

    add testapps/

---
+++ testapps/PY/teacher.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from person import Person
+from pinpoint.plugins.PinpointCommonPlugin import PinpointCommonPlugin
+
+
+class Teacher(Person):
+
+    @PinpointCommonPlugin("Teacher", __name__)
+    def eat(self):
+        return super().eat()+"Teacher eating too!"
