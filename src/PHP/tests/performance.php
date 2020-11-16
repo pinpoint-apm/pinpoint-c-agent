be1e861037bfe8c41cdc1b86bb855ad6c9a8afbb
eeliu <eeliu2009@gmail.com>
2020-11-16 15:41:46 +0800

    performance test

---
+++ src/PHP/tests/performance.php
@@ -0,0 +1,16 @@
+<?php
+
+for($i=0;$i<1000000;$i++){
+    // $id = pinpoint_start_trace(0);
+    // pinpoint_add_clue("name","Foo",$id);
+    // $id = pinpoint_start_trace($id);
+    // pinpoint_add_clues("global","global_value",$id,1);
+    // pinpoint_add_clue("global","global_value",$id,1);
+    // pinpoint_add_clues("name","Foo1",$id);
+    // $id = pinpoint_start_trace($id);
+    // pinpoint_add_clue("name","Foo2",$id);
+    // $id = pinpoint_end_trace($id);
+    // $id = pinpoint_end_trace($id);
+    // $id = pinpoint_end_trace($id);
+    
+}
