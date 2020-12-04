749c75bdd2c916fb4f38ff6ba37500d265d7a2ce
eeliu <eeliu2009@gmail.com>
2020-12-04 11:41:01 +0800

    style: add copyright

--- plugins/PY2.7/pinpoint/libs/pymemcache/test.py
+++
@@ -1,5 +0,0 @@
-from pymemcache.client.base import Client
-
-client = Client('localhost')
-client.set('some_key', 'some_value')
-result = client.get('some_key')
--- plugins/PY2.7/pinpoint/libs/python-memcached/test.py
+++
@@ -1,4 +0,0 @@
-from memcache import Client
-
-mc = Client(["127.0.0.1:11211"], debug=1)
-mc.get("ok")
