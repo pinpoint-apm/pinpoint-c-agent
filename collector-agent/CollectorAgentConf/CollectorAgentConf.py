723e6054ac5945263454e2277241270574392538
eeliu <eeliu2009@gmail.com>
2021-01-21 12:19:14 +0800

    feature: env
    - read configuration from system variables

--- collector-agent/CollectorAgentConf/CollectorAgentConf.py
+++ collector-agent/CollectorAgentConf/CollectorAgentConf.py
@@ -17,12 +17,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # ------------------------------------------------------------------------------
+import os
 import time
 
 from CollectorAgent.GrpcAgentImplement import GrpcAgentImplement
 from CollectorAgent.ThriftAgentImplement import ThriftAgentImplement
 from Common.Logger import set_logger_file, set_logger_level, logger_enable_console
-from PinpointAgent.Type import PHP
 from PinpointAgent.Type import SUPPORT_THRIFT, SUPPORT_GRPC
 
 AGENT_VERSION = '1.8.0-RC1'
@@ -47,6 +47,14 @@ class CollectorAgentConf(object):
         self.collector_implement = None
         self.startTimestamp = int(time.time() * 1000)
         self.config = config
+        self.version = AGENT_VERSION
+
+        if not config:
+            ## update system evn and it own a higher priority
+            ## while, only support grpc
+            self._parseSysEnv()
+            return
+
         if config.has_option('Collector', 'collector.grpc.agent.ip') and \
                 config.has_option('Collector', 'collector.grpc.stat.ip') and \
                 config.has_option('Collector', 'collector.grpc.span.ip'):
@@ -62,10 +70,18 @@ class CollectorAgentConf(object):
                                                'collector.grpc.agent.ip')
             self.CollectorAgentPort = config.getint('Collector',
                                                     'collector.grpc.agent.port')
+
             self.max_pending_size = config.getint('Collector',
                                                   'collector.grpc.discardpolicy.maxpendingthreshold',fallback=10000)
+
+            agentIsDocker = config.get('Collector',
+                                       'collector.agent.isdocker', fallback='false')
+
+            self.agentIsDocker = True if agentIsDocker.lower() == 'true' else False
+
             self.collector_type = SUPPORT_GRPC
             self.collector_implement = GrpcAgentImplement
+
         else:
             self.CollectorSpanIp = config.get('Collector',
                                               'CollectorSpanIp')
@@ -82,15 +98,34 @@ class CollectorAgentConf(object):
             self.collector_type = SUPPORT_THRIFT
             self.collector_implement = ThriftAgentImplement
 
-        self.version = AGENT_VERSION
+        self.log_dir = config.get('Common', 'LOG_DIR', fallback='/tmp')
+        self.log_level = config.get('Common', 'Log_Level', fallback='DEBUG')
+        self.webPort = self.config.get('Common', 'Web_Port')
 
-        log_dir = config.get('Common', 'LOG_DIR', fallback=None)
-        log_level = config.get('Common', 'Log_Level', fallback='DEBUG')
-        if log_dir is not None:
-            set_logger_file(log_dir)
+    def _parseSysEnv(self):
+        try:
+            self.CollectorSpanIp = os.environ['PP_COLLECTOR_AGENT_SPAN_IP']
+            self.CollectorSpanPort = int(os.environ['PP_COLLECTOR_AGENT_SPAN_PORT'])
+            self.CollectorAgentIp = os.environ['PP_COLLECTOR_AGENT_AGENT_IP']
+            self.CollectorAgentPort = int(os.environ['PP_COLLECTOR_AGENT_AGENT_PORT'])
+            self.CollectorStatIp = os.environ['PP_COLLECTOR_AGENT_STAT_IP']
+            self.CollectorStatPort = int(os.environ['PP_COLLECTOR_AGENT_STAT_PORT'])
+            self.agentIsDocker = True if os.environ.get('PP_COLLECTOR_AGENT_ISDOCKER').lower() == 'true' else False
+            self.log_dir = os.getenv('PP_LOG_DIR', '/tmp/')
+            self.log_level = os.getenv('PP_Log_Level', 'DEBUG')
+            self.webPort = os.getenv('PP_WEB_PORT', '80')
+            self.collector_type = SUPPORT_GRPC
+            self.collector_implement = GrpcAgentImplement
+        except KeyError as e:
+            # an exception is convenience for debugging
+            raise RuntimeError('Please check your environment variable %s is missing' % (e))
+
+    def _setLog(self, dir, level):
+        if dir is not None:
+            set_logger_file(dir)
         else:
             logger_enable_console()
-        set_logger_level(log_level)
+        set_logger_level(level)
 
     def getSpanHost(self):
         return (self.CollectorSpanIp, self.CollectorSpanPort)
@@ -102,7 +137,7 @@ class CollectorAgentConf(object):
         return (self.CollectorAgentIp, self.CollectorAgentPort)
 
     def getWebPort(self):
-        return self.config.get('Common', 'Web_Port')
+        return self.webPort
 
     def clean(self):
         pass
