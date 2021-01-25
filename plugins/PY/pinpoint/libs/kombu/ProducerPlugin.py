453aba3515fb860891d335376dd8122e076e670c
eeliu <eeliu2009@gmail.com>
2021-01-25 13:30:56 +0800

    fix: celery plugins
    - unkonw node

--- plugins/PY/pinpoint/libs/kombu/ProducerPlugin.py
+++ plugins/PY/pinpoint/libs/kombu/ProducerPlugin.py
@@ -2,19 +2,6 @@
 # -*- coding: UTF-8 -*-
 # Created by eeliu at 12/29/20
 
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-# ******************************************************************************
 
 # ******************************************************************************
 #   Copyright  2020. NAVER Corp.
@@ -53,8 +40,6 @@ class ProducerPlugin(Candy):
             pinpointPy.add_clues(PP_RABBITMQ_ROUTINGKEY, kwargs['routing_key'])
 
         generatePPRabbitMqHeader(kwargs['headers'])
-
-        print(kwargs)
 
         uri = args[0].connection.as_uri()
         pinpointPy.add_clue(PP_DESTINATION, str(uri))
