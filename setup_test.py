48e34ddcc2d42d1935d967c1d8241ec57198d6ce
su-wei <su.wei@navercorp.com>
2021-07-27 14:56:01 +0800

    Add action for release pinpointPy

--- setup_test.py
+++ setup_test.py
@@ -67,7 +67,7 @@ else:
 
 
 setup(name='pinpointPy',
-      version="1.0.5",
+      version="1.0.12",
       author="eeliu", 
       author_email='eeliu2009@gmail.com',
       license='Apache License 2.0',
