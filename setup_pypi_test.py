ec9eda0269806a7c62b87ec8b026a3e476500279
eeliu <27064129+eeliu@users.noreply.github.com>
2022-01-13 15:10:12 +0800

    [ci] support pypi (#393)

--- setup_pypi_test.py
+++ setup_pypi_test.py
@@ -67,7 +67,7 @@ else:
 
 
 setup(name='pinpointPy',
-      version="1.0.13",
+      version="1.0.14",
       author="eeliu", 
       author_email='eeliu2009@gmail.com',
       license='Apache License 2.0',
