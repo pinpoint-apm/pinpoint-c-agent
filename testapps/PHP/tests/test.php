3da1a44eff84f7cb4c80de7ed6c2ef50c2968d7d
Evelyn <41946743+EyelynSu@users.noreply.github.com>
2022-02-15 17:38:35 +0800

    Update test.php

--- testapps/PHP/tests/test.php
+++ testapps/PHP/tests/test.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = 'mysql:host=localhost;port=3306';
+$dsn = 'mysql:host=127.0.0.1;port=3306';
 $con = new PDO($dsn, "root","123456");
 $sql = 'show databases;';
 var_dump($con->query($sql));
