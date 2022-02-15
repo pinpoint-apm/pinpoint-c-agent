0b3e547e19fbd6f68233856c171b562eb8345429
Evelyn <41946743+EyelynSu@users.noreply.github.com>
2022-02-15 10:35:57 +0800

    Create script.sql

---
+++ testapps/PHP/tests/script.sql
@@ -0,0 +1,6 @@
+CREATE TABLE user( ".
+        "id INT NOT NULL AUTO_INCREMENT, ".
+        "name VARCHAR(100) NOT NULL, ".
+        "age VARCHAR(40) NOT NULL, ".
+        "city VARCHAR(40) NOT NULL,, ".
+        "PRIMARY KEY ( id ))ENGINE=InnoDB DEFAULT CHARSET=utf8;
