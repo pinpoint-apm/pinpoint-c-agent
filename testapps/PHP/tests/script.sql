7f4f8c116fd3388ef5c3a9620429c44ef4453ac8
Evelyn <41946743+EyelynSu@users.noreply.github.com>
2022-02-15 17:08:45 +0800

    Update script.sql

--- testapps/PHP/tests/script.sql
+++ testapps/PHP/tests/script.sql
@@ -1,6 +1,8 @@
-CREATE TABLE user( ".
-        "id INT NOT NULL AUTO_INCREMENT, ".
-        "name VARCHAR(100) NOT NULL, ".
-        "age VARCHAR(40) NOT NULL, ".
-        "city VARCHAR(40) NOT NULL,, ".
-        "PRIMARY KEY ( id ))ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `user` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(100) NOT NULL,
+        `age` INT,
+        `city` VARCHAR(100),
+	PRIMARY KEY (`id`)
+);
+ALTER TABLE user ADD UNIQUE KEY id_idx (id);
