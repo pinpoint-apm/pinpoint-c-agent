5d1c43fd10c84b580848bd05e4d7bbd21dcfc703
Evelyn <41946743+EyelynSu@users.noreply.github.com>
2022-02-16 09:56:32 +0800

    Update script.sql

--- testapps/PHP/tests/script.sql
+++ testapps/PHP/tests/script.sql
@@ -1,3 +1,8 @@
+CREATE DATABASE DBTest;
+CREATE USER 'test'@'%' IDENTIFIED BY '123456';
+GRANT ALL ON *.* TO 'test'@'%' IDENTIFIED BY '123456';
+flush privileges;
+use DBTest;
 CREATE TABLE `user` (
         `id` INT NOT NULL AUTO_INCREMENT,
         `name` VARCHAR(100) NOT NULL,
