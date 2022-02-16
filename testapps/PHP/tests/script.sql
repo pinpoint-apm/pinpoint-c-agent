49217582ef6f52a34627c03d06df1174163dfc5f
Evelyn <41946743+EyelynSu@users.noreply.github.com>
2022-02-16 10:23:11 +0800

    Update script.sql

--- testapps/PHP/tests/script.sql
+++ testapps/PHP/tests/script.sql
@@ -2,12 +2,12 @@ CREATE DATABASE DBTest;
 CREATE USER 'test'@'%' IDENTIFIED BY '123456';
 GRANT ALL ON *.* TO 'test'@'%' IDENTIFIED BY '123456';
 flush privileges;
--- use DBTest;
--- CREATE TABLE `user` (
---         `id` INT NOT NULL AUTO_INCREMENT,
---         `name` VARCHAR(100) NOT NULL,
---         `age` INT,
---         `city` VARCHAR(100),
--- 	PRIMARY KEY (`id`)
--- );
--- ALTER TABLE user ADD UNIQUE KEY id_idx (id);
+use DBTest;
+CREATE TABLE `user` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(100) NOT NULL,
+        `age` INT,
+        `city` VARCHAR(100),
+	PRIMARY KEY (`id`)
+);
+ALTER TABLE user ADD UNIQUE KEY id_idx (id);
