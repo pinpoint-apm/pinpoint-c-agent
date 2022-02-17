7b1dfd69c6a12f2ebd84cf7c6cbdff235fe41615
Evelyn <41946743+EyelynSu@users.noreply.github.com>
2022-02-17 10:07:22 +0800

    update thinkphp5.0 to 5.1 (#401)

--- testapps/PHP/public/index.php
+++ testapps/PHP/public/index.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2018 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
@@ -10,9 +10,21 @@
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
+namespace think;
 
-// 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
+// 加载基础文件
+require __DIR__ . '/../thinkphp/base.php';
 
-// 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
+//###################################################################
+define('AOP_CACHE_DIR', __DIR__ . '/../Cache/');
+define('PLUGINS_DIR', __DIR__ . '/../Plugins/');
+define('APPLICATION_NAME','thinkphp5.1'); // your application name
+define('APPLICATION_ID','thinkphp5.1');  // your application id
+define('PINPOINT_USE_CACHE','YES');
+require_once __DIR__ . '/../vendor/pinpoint-apm/pinpoint-php-aop/auto_pinpointed.php';
+//###################################################################
+
+// 支持事先使用静态方法设置Request对象和Config对象
+
+// 执行应用并响应
+Container::get('app')->run()->send();
