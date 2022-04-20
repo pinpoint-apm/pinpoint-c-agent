92c55a816c7cb6b31d38ca559d2ed0fd63f3e2ee
eeliu <27064129+eeliu@users.noreply.github.com>
2022-04-20 11:36:48 +0800

    * remove common/

--- plugins/PY/pinpointPy/__init__.py
+++ plugins/PY/pinpointPy/__init__.py
@@ -14,12 +14,12 @@
 #   limitations under the License.
 # ******************************************************************************
 
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # Created by eeliu at 2/4/21
 __version__ = "0.0.5"
-from .libs import monkey_patch_for_pinpoint
-from .common import APP_ID,APP_NAME
-from _pinpointPy import set_agent
 
-__all__= ['monkey_patch_for_pinpoint','APP_ID','APP_NAME','set_agent']
+from .libs import monkey_patch_for_pinpoint
+from .pinpoint import set_agent
+
+__all__ = ['monkey_patch_for_pinpoint', 'set_agent']
