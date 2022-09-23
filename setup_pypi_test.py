787acb7c11f4727df95d48b3c8e7254f7c592bab
eeliu <27064129+eeliu@users.noreply.github.com>
2022-09-23 17:30:45 +0800

    missing a file (#481)

--- setup_pypi_test.py
+++ setup_pypi_test.py
@@ -67,7 +67,7 @@ else:
 
 
 setup(name='pinpointPy',
-      version="1.0.27",
+      version="1.0.28",
       author="cd_pinpoint members", 
       author_email='dl_cd_pinpoint@navercorp.com',
       license='Apache License 2.0',
