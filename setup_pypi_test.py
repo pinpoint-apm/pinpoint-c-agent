03bd7145cbc17d7e9f3b2622524cfcf2d2ae64e5
eeliu <27064129+eeliu@users.noreply.github.com>
2022-10-21 08:43:49 +0800

    update to 1.0.32

--- setup_pypi_test.py
+++ setup_pypi_test.py
@@ -67,7 +67,7 @@ else:
 
 
 setup(name='pinpointPy',
-      version="1.0.31",
+      version="1.0.32",
       author="cd_pinpoint members", 
       author_email='dl_cd_pinpoint@navercorp.com',
       license='Apache License 2.0',
