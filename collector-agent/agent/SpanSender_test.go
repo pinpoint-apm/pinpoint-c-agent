6d26504288829d8c2d7459d7d43c885eb257b5d4
Ohjun Kwon <dev@kojandy.com>
2026-08-18 16:30:52 +0900

    fix: generate 16-byte sqlUids

--- collector-agent/agent/SpanSender_test.go
+++ collector-agent/agent/SpanSender_test.go
@@ -15,7 +15,7 @@ func TestSqlUidFormat(t *testing.T) {
 
 	id := sender.getSqlUidMetaApiId("INSERT INTO chengji_m VALUES (%s, %s, %s)")
 	t.Logf("%v", string(id))
-	if len(id) == 0 {
-		t.Error(id)
+	if len(id) != 16 {
+		t.Errorf("sqlUid length = %d, want 16 (%x)", len(id), id)
 	}
 }
